e52331005c078cb5e428d819707782f75a24f194
Esteph Mendoza <99153636+EstephMenC@users.noreply.github.com>
2025-04-11 22:18:41 -0700

    Se añada variable e
    la variable se utiliza para dividir la variable d

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,11 +1,13 @@
 # Prueba para GIT
 # Ejemplo 
 # Calculo 1
+# Se añade variable e
 a = 30
 b = 5
 c = 30 * 5
+e = 4
 print ('El resultado es', c)
 # Añadir variable d
 # Prueba para ST
-d = c * 10
+d = (c * 10)/e
 print ('El resultado total =' d)
